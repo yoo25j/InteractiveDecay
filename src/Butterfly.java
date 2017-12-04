86c8fc9ae23dd7ce9df699e33fe7f1168c9ef074
Nikkole Spencer <spenc22n@mtholyoke.edu>
2017-12-04 15:35:38 -0500

    merge
    merge

---
+++ src/Butterfly.java
@@ -0,0 +1,45 @@
+
+
+import processing.core.*;
+
+public class Butterfly extends PApplet {
+	PImage img;       // The source image
+
+	public void settings() {
+		  size(1024,764, P3D);
+		}
+	public void setup() {
+ 
+	  img  = loadImage("LargeButterfly.jpg"); // Load the image
+	}
+
+	public void draw() {
+	  background(0);
+	  loadPixels();
+	  // Begin loop for columns
+	  for ( int i = 0; i < img.width;i++) {
+	    // Begin loop for rows
+	    for ( int j = 0; j < img.height;j++) {
+	      int x = width * i/img.width; // x position
+	      int y = height * j/img.height; // y position
+	      int loc = i + j*img.width;           // Pixel array location
+	      int c = img.pixels[loc];       // Grab the color
+	      // Calculate a z position as a function of mouseX and pixel brightness
+	      float z = (float) ((mouseX/(float)width) * brightness(img.pixels[loc]) - 100.0);
+	      // Translate to the location, set fill and stroke, and draw the rect
+	      pushMatrix();
+	      translate(x,y,z);
+	      fill(c);
+	      noStroke();
+	      rectMode(CENTER);
+	      rect(0,0,width/img.width,height/img.height);
+	      popMatrix();
+	    }
+	  }
+	}
+	public static void main(String[] args) {
+		PApplet.main(Butterfly.class.getName());
+
+	}
+
+}
