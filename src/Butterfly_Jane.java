84847d30a92f49713acce7f7e35e8957cbad1411
yoo25j <yoo25j@mtholyoke.edu>
2017-12-06 16:24:26 -0500

    ignore
    ignoe

--- src/Butterfly_Jane.java
+++ src/Butterfly_Jane.java
@@ -4,7 +4,6 @@ import processing.core.PImage;
 import processing.core.PVector;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import processing.core.*;
 
 public class Butterfly_Jane extends PApplet {
@@ -18,8 +17,9 @@ public class Butterfly_Jane extends PApplet {
 
 	public void settings() {
 		//size(1024,764, P3D);
-		size(1000,1000, P3D);
+		size(800,800, P3D);
 	}
+
 
 	public void setup() {
 		butterfly_img  = loadImage("butterfly.gif"); // Load the image
@@ -32,6 +32,36 @@ public class Butterfly_Jane extends PApplet {
 		KinectBodyData bodyData = kinectReader.getMostRecentData();
 		Body person = bodyData.getPerson(0);	
 		
+		background(0);
+		loadPixels();
+	
+	  // Begin loop for columns 
+	  for ( int i = 0; i < butterfly_img.width;i++) {
+	    // Begin loop for rows
+	    for ( int j = 0; j < butterfly_img.height;j++) {
+	      float x = width * i/butterfly_img.width; // x position
+	      float y = height * j/butterfly_img.height; // y position
+	      int loc = i + j*butterfly_img.width;           // Pixel array location
+	      int c = butterfly_img.pixels[loc];       // Grab the color
+	      
+//	      if(person != null){
+//				PVector handLeft = person.getJoint(Body.HAND_LEFT);
+//				PVector handRight = person.getJoint(Body.HAND_RIGHT);
+	      
+	      // Calculate a z position as a function of mouseX and pixel brightness
+	      float z = (float) ((mouseX/(float)width) * brightness(butterfly_img.pixels[loc]) - 100.0);
+	      // Translate to the location, set fill and stroke, and draw the rect
+	      pushMatrix();
+	      translate(x,y,z + mouseY );
+	      fill(c);
+	      noStroke();
+	      rectMode(CENTER);
+	      rect(0,0,width/butterfly_img.width,height/butterfly_img.height);
+	      popMatrix();
+	    }
+	  
+		}
+
 		if(person != null){
 			PVector head = person.getJoint(Body.HEAD);
 			PVector spine = person.getJoint(Body.SPINE_SHOULDER);
