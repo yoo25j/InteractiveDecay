67106f2335c6af539524c98aa9f6abb25deb7470
yoo25j <yoo25j@mtholyoke.edu>
2017-12-10 17:55:36 -0500

    formatting issues & sound jar

--- src/Butterfly_Jane.java
+++ src/Butterfly_Jane.java
@@ -4,10 +4,9 @@ import processing.core.PImage;
 import processing.core.PVector;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import processing.core.*;
 
-import ddf.minim.*;
+//import ddf.minim.*;
 
 public class Butterfly_Jane extends PApplet {
 
@@ -37,7 +36,6 @@ public class Butterfly_Jane extends PApplet {
 	}
 
 	public void settings() {
-
 		// size(1024,764, P3D);
 		createWindow(true, true, .5f);
 
@@ -46,19 +44,18 @@ public class Butterfly_Jane extends PApplet {
 	public void setup() {
 		butterfly_img = loadImage("butterfly.gif"); // Load the image
 		// loadImage("butterfly.gif"); // Load the image
-
 		kinectReader = new KinectBodyDataProvider(8008);
 		kinectReader.start();
 	}
 
 	public void draw() {
 		KinectBodyData bodyData = kinectReader.getMostRecentData();
-
 		Body person = bodyData.getPerson(0);
 
 		background(0);
 		loadPixels();
-		if (person != null) {
+		
+		if (person != null) { 
 			PVector handLeft = person.getJoint(Body.HAND_LEFT);
 			PVector handRight = person.getJoint(Body.HAND_RIGHT);
 			if (handRight != null && handLeft != null) {
@@ -66,7 +63,8 @@ public class Butterfly_Jane extends PApplet {
 				rep_Y = handLeft.y * 750;
 			}
 		}
-		// Begin loop for columns
+
+
 		for (int i = 0; i < butterfly_img.width; i++) {
 			// Begin loop for rows
 			for (int j = 0; j < butterfly_img.height; j++) {
@@ -74,7 +72,6 @@ public class Butterfly_Jane extends PApplet {
 				float y = height * j / butterfly_img.height; // y position
 				int loc = i + j * butterfly_img.width; // Pixel array location
 				int c = butterfly_img.pixels[loc]; // Grab the color
-
 				// Calculate a z position as a function of mouseX and pixel brightness
 				float z = (float) ((rep_X / (float) width) * brightness(butterfly_img.pixels[loc]) - 100.0);
 				// Translate to the location, set fill and stroke, and draw the rect
@@ -91,7 +88,6 @@ public class Butterfly_Jane extends PApplet {
 
 	public static void main(String[] args) {
 		PApplet.main(Butterfly_Jane.class.getName());
-
 	}
 
 }
